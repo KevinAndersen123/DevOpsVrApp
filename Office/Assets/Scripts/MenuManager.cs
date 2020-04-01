98aab0f9019107fffc9dd34174fc545785cef52e
Kevin Andersen <c00225541@itcarlow.ie>
2020-04-01 10:46:45 +0100

    Added Help canvas and team station

--- Office/Assets/Scripts/MenuManager.cs
+++ Office/Assets/Scripts/MenuManager.cs
@@ -6,8 +6,11 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject m_player;
-    private Vector3 spawnPlayer = new Vector3(-20, 1.8f, 30.0f);
+    private bool helpActive = false;
 
+    private Vector3 spawnPlayer = new Vector3(-20, 1.8f, 30.0f);
+    [SerializeField]
+    private GameObject helpCanvas;
     [SerializeField]
     private GameObject canvas;
     [SerializeField]
@@ -23,4 +26,19 @@ public class MenuManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void Help()
+    {
+        if(!helpActive)
+        {
+            helpCanvas.SetActive(true);
+            helpActive = true;
+        }
+        else
+        {
+            helpCanvas.SetActive(false);
+            helpActive = false;
+        }
+    }
+       
 }
---
+++ Office/Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject text;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            text.SetActive(false);
+        }
+    }
+}
