f15e1ea3d206bb260c39d7bb155e3f25ea62f177
Kevin Andersen <c00225541@itcarlow.ie>
2020-05-12 16:16:40 +0100

    Added shapes spawning with vr buttons(subject to testing in vr)

---
+++ Office/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class ButtonController : MonoBehaviour
+{
+    Transform parent;
+    public Transform pointA;
+    public Transform pointB;
+
+    public UnityEvent ReleasedA;
+    public UnityEvent ReleasedB;
+    public UnityEvent HitA;
+    public UnityEvent HitB;
+    Vector3 offset = new Vector3(0, 0, 0);
+    int state = 0;
+    int prevState = 0;
+    public GameObject spawnPos;
+    public GameObject shapePrefab;
+    public void SpawnShape()
+    {
+        Instantiate(shapePrefab, spawnPos.transform.position, shapePrefab.transform.rotation);
+    }
+    void Update()
+    {
+        if (parent != null)
+        {
+            transform.position = ClosestPointOnLine(parent.position) - offset;
+        }
+        else
+        {
+            transform.position = ClosestPointOnLine(transform.position) - offset;
+        }
+        if (transform.position == pointA.position)
+        {
+            state = 1;
+        }
+        else if (transform.position == pointB.position)
+        {
+            state = 2;
+        }
+        else
+            state = 0;
+
+        prevState = state;
+        if(state == 1 && prevState == 0)
+        {
+            HitA.Invoke();
+        }
+        else if(state == 2 && prevState == 0)
+        {
+            HitB.Invoke();
+        }
+        else if(state== 0 && prevState==1)
+        {
+            ReleasedA.Invoke();
+        }
+        else if (state == 0 && prevState == 2)
+        {
+            ReleasedB.Invoke();
+        }
+        //for testing spawn without any headset
+        if(Input.anyKeyDown)
+        {
+            HitB.Invoke();
+        }
+    }
+
+
+    Vector3 ClosestPointOnLine(Vector3 point)
+    {
+        Vector3 va = pointA.position + offset;
+        Vector3 vb = pointB.position + offset;
+
+        Vector3 vVector1 = point - va;
+
+        Vector3 vVector2 = (vb - va).normalized;
+
+        float t = Vector3.Dot(vVector2, vVector1);
+
+        if (t <= 0)
+            return va;
+
+        if (t >= Vector3.Distance(va, vb))
+            return vb;
+
+        Vector3 vVector3 = vVector2 * t;
+
+        Vector3 vClosestPoint = va + vVector3;
+
+        return vClosestPoint;
+    }
+}
+
---
+++ Office/Assets/Scripts/ShapeSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour
+{
+    public Vector3 spawnPos;
+    public GameObject[] shapes;
+    public void SpawnShape(int shapeIndex)
+    {
+        Instantiate(shapes[shapeIndex], spawnPos, transform.rotation);
+    }
+}
