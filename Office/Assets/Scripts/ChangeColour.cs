daab0017b66c97e2b88eb769aa8e901a33641eb2
Kevin Andersen <c00225541@itcarlow.ie>
2020-05-13 15:02:14 +0100

    Change colour station added

---
+++ Office/Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+    public Material material;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Square") || other.gameObject.CompareTag("Circle") || other.gameObject.CompareTag("Triangle"))
+        {
+            other.gameObject.GetComponent<Renderer>().sharedMaterial = material;
+        }
+    }
+}
