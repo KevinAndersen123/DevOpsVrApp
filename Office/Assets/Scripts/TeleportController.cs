b2e759c5cbd9ad0b9192e15a2bb754be5ffd9986
PatrickDarcy <C00226157@itcarlow.ie>
2020-05-13 10:06:10 +0100

    Teleportation in progress

---
+++ Office/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject m_player;
+    public List<GameObject> teleportLocations;
+
+    public void teleportTo(int location)
+    {
+        m_player.transform.position = new Vector3(teleportLocations[location].transform.position.x,
+                                                  teleportLocations[location].transform.position.y,
+                                                  teleportLocations[location].transform.position.z);
+    }
+}
